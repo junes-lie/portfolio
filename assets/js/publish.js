bb2d5f4a7e7bcc475fdf30dc7163140bb4aae1f9
junes-lie <sage0530@naver.com>
2026-02-11 02:53:04 +0900

    0210 퍼블 백그라운드가 바뀌도록 js수정

--- assets/js/publish.js
+++ assets/js/publish.js
@@ -1,8 +1,15 @@
+const section = document.querySelector('#publishing');
+const sectionBg = section.querySelector('.section-bg');
 
-const cards = document.querySelectorAll('#publishing .card');
-const indicators = document.querySelectorAll('#publishing .indicator');
+const cards = section.querySelectorAll('.card');
+const indicators = section.querySelectorAll('.indicator');
 let activeIndex = 0;
 let autoPlayInterval;
+
+function getTargetSrc(card) {
+  const isPortrait = window.matchMedia("(orientation: portrait)").matches;
+  return isPortrait ? card.dataset.bgMo : card.dataset.bgPc;
+}
 
 function updateCards() {
   const total = cards.length;
@@ -11,19 +18,26 @@ function updateCards() {
     let relativeIndex = (index - activeIndex + total) % total;
     
     if (relativeIndex === 0) {
+      card.classList.add('active');
       card.style.zIndex = '20';
       card.style.opacity = '1';
       card.style.transform = 'translateX(0%) scale(1) rotate(0deg)';
       card.style.filter = 'brightness(100%)';
+
+      const finalSrc = getTargetSrc(card);
+      sectionBg.style.backgroundImage = `url('${finalSrc}')`;
+      
     } else if (relativeIndex === 1) {
+      card.classList.remove('active');
       card.style.zIndex = '10';
       card.style.opacity = '0.8';
-      card.style.transform = 'translateX(60%) scale(0.85) rotate(15deg)';
+      card.style.transform = 'translateX(90%) translateY(10%) scale(0.85) rotate(15deg)';
       card.style.filter = 'brightness(50%)';
     } else {
+      card.classList.remove('active');
       card.style.zIndex = '10';
       card.style.opacity = '0.8';
-      card.style.transform = 'translateX(-60%) scale(0.85) rotate(-15deg)';
+      card.style.transform = 'translateX(-90%) translateY(10%) scale(0.85) rotate(-15deg)';
       card.style.filter = 'brightness(50%)';
     }
   });
